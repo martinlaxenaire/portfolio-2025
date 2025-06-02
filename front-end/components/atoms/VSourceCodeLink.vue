ecbc2347996941604d6495d055bb2346a59e84ff
martinlaxenaire <martin.laxenaire@gmail.com>
2025-06-02 09:56:35 +0200

    - source code link component

---
+++ front-end/components/atoms/VSourceCodeLink.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup></script>
+
+<template>
+  <a
+    href="https://github.com/martinlaxenaire/portfolio-2025"
+    target="_blank"
+    rel="noopener"
+    :class="$style.root"
+  >
+    &lt;/&gt;
+  </a>
+</template>
+
+<style lang="scss" module>
+.root {
+}
+</style>
